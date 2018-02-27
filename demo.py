ac95a3854b0bb554b9b99da5734d16cb58085ccf
gongzhihao <>
2018-02-27 18:29:38 +0900

    single thread for user names

--- demo.py
+++ demo.py
@@ -1,9 +1,29 @@
-import urllib
 import urllib2
+import xlwt
+from bs4 import BeautifulSoup
 
-values = {"username":"aria_m1911@126.com","password":"618618618Qw+"}
-data = urllib.urlencode(values)
-url = "http://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
-request = urllib2.Request(url,data)
-response = urllib2.urlopen(request)
-print response.read()
+name_array = [];
+book = xlwt.Workbook(encoding='utf-8',style_compression=0)
+sheet = book.add_sheet('user_name',cell_overwrite_ok=True)
+
+for num in range(10000,10020):
+
+    request = urllib2.Request("https://gamewith.jp/user/profile/"+str(num))
+    response = urllib2.urlopen(request)
+
+    soup = BeautifulSoup(response)
+
+    name = soup.select('div[class="_name fll"]')[0].get_text()
+
+    name = name.strip()
+
+    name_array.append(name)
+
+
+print name_array[0]
+
+for i, row in enumerate(name_array):
+    sheet.write(i, 0, row)
+
+book.save('/Users/gongzhihao/Desktop/names.xls')
+
